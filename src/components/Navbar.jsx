c0e53a7dccf102e5d31a079f1405fd880b83f8cb
TalissonMelo <talissonmelorodrigues@gmail.com>
2025-01-18 07:39:22 -0300

    Flex Layouts

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Flex, Heading, Box, Text, Button, Spacer, HStack } from "@chakra-ui/react"
+
+export default function Navbar() {
+  return (
+    <Flex as="nav" p="10px" alignItems="center">
+      <Heading as="h1">@Chakra ui</Heading>
+      <Spacer />
+
+      <HStack spacing="20px"> 
+        <Box bg="gray.200" p="10px">T</Box>
+        <Text>talissonmelo@</Text>
+        <Button colorScheme="purple">Logout</Button>
+      </HStack>
+    </Flex>
+  )
+}
